3e162cb289eec9fcc072baff0667e21b2feb967f
Nooaah <noah.chtl@gmail.com>
2020-02-03 11:26:11 +0100

    Q3) Add method join()

--- TP1/Main.java
+++ TP1/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        MyThread t = new MyThread();
-        t.start();
+        MyThread t1 = new MyThread();
+        t1.start();
+        MyThread t2 = new MyThread();
+        t2.start();
+        MyThread t3 = new MyThread();
+        t3.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch(InterruptedException exc) {
+        }
+        System.out.println("Fin des Threads");
+        
+    
     }
 }
