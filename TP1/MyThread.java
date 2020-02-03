232eff5b8080e9d9f426bd2af480c9a588c28969
Nooaah <noah.chtl@gmail.com>
2020-02-03 11:12:21 +0100

    first commit

---
+++ TP1/MyThread.java
@@ -0,0 +1,14 @@
+import java.lang.Thread;
+
+public class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Thread " + i + " en execution");
+            try {
+                sleep((int)(Math.random() * 1000));
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
